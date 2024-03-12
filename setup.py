5405b262576d6d928106c8727ea0f53b91616d67
Sushmita <sushmita.iar0@gmail.com>
2024-03-12 21:40:08 +0545

    update dateparser

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     install_requires=[
         'beautifulsoup4==4.11.1',
         'cloudpickle==2.2.0',
-        'dateparser==1.1.3',
+        'dateparser==1.2.0',
         'elasticsearch==8.5.0',
         'gensim==4.3.2',
         'importlib-metadata==5.0.0',
